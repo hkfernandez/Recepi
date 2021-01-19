878083f4f6fe470cc88527c1a6c8ed2b1dbfae23
omar khalif <73026620+Cumar-khalif@users.noreply.github.com>
2021-01-19 07:15:07 -0600

    read me file

--- assets/script/script.js
+++ assets/script/script.js
@@ -425,9 +425,11 @@ console.log('is working');
 
 function convertUnit(ingredient, amount, initialUnit, targetUnit) {
     var urlConvertUnit = `https://api.spoonacular.com/recipes/convert?ingredientName=${ingredient}&sourceAmount=${amount}&sourceUnit=${initialUnit}&targetUnit=${targetUnit}`;
-    var API_KEY = 'd3a8582988694e8780200641aad4694b'
+    var API_KEY = 'd3a8582988694e8780200641aad4694b' 
+    // (Spoonacular Keys)
+    
 
-    var API_KEY2 = 'eedc150ed7msh5507510bf70abc4p19b8a9jsn8b034ed95169'
+    // var API_KEY2 = 'eedc150ed7msh5507510bf70abc4p19b8a9jsn8b034ed95169'
 
     const settings = {
         "async": true,
